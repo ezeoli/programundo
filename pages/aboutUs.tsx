b9e242bfcc397619abbdf0b5474b020cba17c394
matias <matiasrozas.lp@gmail.com>
2023-06-09 05:11:25 -0300

    link aboutMe back to homepage

--- pages/aboutUs.tsx
+++ pages/aboutUs.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import Image from 'next/image'
+import '../app/globals.css'
+import Link from 'next/link'
 
 type Props = {}
 
 export default function AboutUs({}: Props) {
   return (
-    <div>AboutUs</div>
+    <>
+     <nav className='flex  bg-gray-400 h-16'> 
+          <Link href={'/'} >
+                  <Image
+                    src="/LOGO.png"
+                    width={100}
+                    height={100}
+                    alt="programundo"
+                    />
+          </Link>
+
+                  <Link href="/aboutUs"
+                    className='flex-auto text-blue-800 p-2'>About us
+                  </Link>
+                  <Link href="/products"
+                    className='flex-auto text-blue-800 p-2'>Our products
+                  </Link>
+                  <Link href="/clients"
+                    className='flex-auto text-blue-800 p-2'>Clients
+                  </Link>
+                  <Link href="/contact"
+                    className='flex-auto text-blue-800 p-2'>Contact
+                  </Link>
+              </nav>
+    <section id="about">
+                    <h2 className="text-2xl font-bold">About us</h2>
+                    
+                    <Image
+                      src="/programundo2.jpg"
+                      width={500}
+                      height={500}
+                      alt="programundo"
+                    />
+                    <p>This is the About section.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem rerum eveniet fuga praesentium asperiores eos deleniti doloremque quibusdam consectetur sunt similique tempora architecto obcaecati maiores, tempore natus aperiam ipsum totam.
+                    </p>
+                    <Link href={'/'}>Go to Home</Link>
+                    
+                </section>
+                </>
   )
 }
