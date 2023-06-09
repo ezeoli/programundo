d594f0110b75285c9419c5852f444bce9b093ca1
matias <matiasrozas.lp@gmail.com>
2023-06-09 10:58:47 -0300

    back to homepage componente Navbar

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar({}: Props) {
   return (
     <nav className="flex  bg-gray-400 h-16">
           <Link href={'/'}>
-          <Image src="/LOGO.png" width={70} height={70} className="p-2" alt="programundo" />
+          <Image src="/LOGO.png" width={70} height={70} className="p-2" alt="programundo"/>
           </Link>
           <Link href="/aboutUs" className="flex-auto text-blue-800 ml-20 p-5">
             About us
