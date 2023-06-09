145f59ac4a992e36f7ef79298fae0f2a949adb75
matias <matiasrozas.lp@gmail.com>
2023-06-09 12:46:58 -0300

    components

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -17,13 +17,13 @@ export default function Navbar({}: Props) {
           alt="programundo"
         />
       </Link>
-      <Link href="#aboutUs" className="flex-auto text-blue-800 ml-20 p-5">
+      <Link href="/#aboutUs" className="flex-auto text-blue-800 ml-20 p-5">
         About us
       </Link>
       <Link href="/products" className="flex-auto text-blue-800 p-5">
         Our products
       </Link>
-      <Link href="#clients" className="flex-auto text-blue-800 p-5">
+      <Link href="/#clients" className="flex-auto text-blue-800 p-5">
         Clients
       </Link>
       <Link href="/contact" className="flex-auto text-blue-800 p-5">
