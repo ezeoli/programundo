33d1d30af228b757e2d1425233954440cd44823b
matias <matiasrozas.lp@gmail.com>
2023-06-09 03:26:26 -0300

    ruteo de paginas

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,16 @@ export default function Home() {
                     height={100}
                     alt="programundo"
                   />
-                  <Link href="#about"
+                  <Link href="/aboutUs"
                     className='flex-auto text-blue-800 p-2'>About us
                   </Link>
-                  <Link href="#products"
+                  <Link href="/products"
                     className='flex-auto text-blue-800 p-2'>Our products
                   </Link>
-                  <Link href="#clients"
+                  <Link href="/clients"
                     className='flex-auto text-blue-800 p-2'>Clients
                   </Link>
-                  <Link href="#contact"
+                  <Link href="/contact"
                     className='flex-auto text-blue-800 p-2'>Contact
                   </Link>
               </nav>
---
+++ pages/clients.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function clients({}: Props) {
+  return (
+    <div>clients</div>
+  )
+}
