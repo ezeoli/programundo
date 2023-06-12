74f39b5022b15b6d566a53b5f3aeee5d130d3126
matias <matiasrozas.lp@gmail.com>
2023-06-12 11:40:53 -0300

    footer

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import "../globals.css";
-import Link from 'next/link';
+import Link from "next/link";
 
-type Props = {}
+type Props = {};
 
 export default function Footer({}: Props) {
   return (
     <>
-        <h1 className="text-blue-800 text-center text-2xl ">
-          We are the next generation of code solutions
-        </h1>
-        <Image
-          src="/programundo3.jpg"
-          width={500}
-          height={500}
-          alt="programundo"
-          className=" rounded "
-          style={{ display: "block", margin: "0 auto" }}
+      <div className=" flex justify-around bg-slate-300">
+
+        <div className="m-5">
+          <Image
+            src="/programundo3.jpg"
+            width={100}
+            height={100}
+            alt="programundo"
+            className="rounded "
+            style={{ display: "block", margin: "0 auto" }}
           />
+          <h1 className="text-blue-800 text-center text-sm ">
+            We are the next generation <br /> of code solutions
+          </h1>
+        </div>
+
+        <div className="m-5">
+          servicio
+        </div>
+
+        <div className="m-5">
+          Newsletter
+        </div>
+      </div>
     </>
-  )
+  );
 }
