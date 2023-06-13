6a99ed8f5a71ca4beb7dd7f047f47d65966d2e46
ezeee <ezeolier@hotmail.com>
2023-06-13 07:54:50 -0300

    responsive todos mismo formato

--- .history/app/Components/Products_20230613073951.tsx
+++ .history/app/Components/Products_20230613073951.tsx
@@ -8,25 +8,21 @@ export default function Products({}: Props) {
   return (
     <section id="products">
     <h2 className="text-4xl font-bold p-5">Our Products</h2>
-    
-    <div className="sm:flex">
     <Image
       src="/programundo4.jpg"
       width="0"
       height="0"
       sizes="100vw"
-      className="rounded-md mx-5 w-1/2 h-auto" 
+      className="w-1/2 h-auto" 
       alt="programundo"
     />
-    <div>
-    <p className="p-10 m-10">
+
+    <p p-10 m-10>
       This is the Products section. Lorem ipsum dolor sit amet consectetur
       adipisicing elit. Iste ab amet quo autem quisquam officia earum
       illo, eaque repellat hic esse quos molestias veniam voluptates, eius
       at similique quibusdam aperiam?
     </p>
-    </div>
-    </div>
   </section>
   )
 }
