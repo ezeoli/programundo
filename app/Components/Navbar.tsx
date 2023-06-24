e6b1845813bbd6e8c8aac05505558fe252660c23
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2023-06-24 12:13:13 -0300

    Actualizar Navbar.tsx

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -87,7 +87,7 @@ export default function Navbar({}: Props) {
               className={`flex-1 justify-self-center pb-3 mt-6 md:block md:pb-0 md:mt-0 
               ${navbar ? "p-12 md:p-0 block" : "hidden"}`}
             >
-              <ul className="flex flex-col md:flex-row h-screen md:h-auto justify-center">
+              <ul className="flex flex-col md:flex-row h-screen md:h-auto items-start justify-center md:items-center">
                 <li className="group relative dropdown">
                   <a
                     className=" pb-2 text-xl text-white py-2 md:px-4 text-center hover:bg-white md:hover:bg-transparent hover:text-black flex gap-2 items-center" >
